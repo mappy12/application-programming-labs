9f419a15d6aeef157d5bebcd2c90f5b3baac2725
mappywin <mappydesuu@gmail.com>
2024-09-29 17:50:09 +0300

    Create a function that downloads cats from Google

--- lab_2/cats.py
+++ lab_2/cats.py
@@ -1,1 +1,12 @@
 from icrawler.builtin import GoogleImageCrawler
+
+def download_cats(keyword: str, num: int, dir: str):
+
+    google_crawler = GoogleImageCrawler(
+        feeder_threads=1,
+        parser_threads=2,
+        downloader_threads=6,
+        storage={'root_dir': dir}
+    )
+
+    google_crawler.crawl(keyword=keyword, max_num=num)
