32a914af05ce79ab4b50eb155f5a49a39ff9c094
mappywin <mappydesuu@gmail.com>
2024-10-16 17:04:24 +0300

    create csv annotation

--- lab_2/csv_cats.py
+++ lab_2/csv_cats.py
@@ -1,12 +1,21 @@
 import csv
 import os
 
+from numpy.ma.core import absolute
+
+
 def create_csv(path_to_images: str, path_to_csv: str) -> None:
 
-    with open('path_to_csv', 'w', newline='', encoding='utf-8') as file:
+    with open("path_to_csv", 'w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
         headers = ['Absolute Path', 'Relative Path']
         writer.writerow(headers)
 
-    for image in os.listdir(path_to_images):
-        relative_path = os.path.relpath(path_to_images, start=im
+        paths = []
+
+        for image in os.listdir(path_to_images):
+
+            relative_path = os.path.join(path_to_images, image)
+            absolute_path = os.path.abspath(relative_path)
+
+            writer.writerow([absolute_path, relative_path])
--- lab_2/main.py
+++ lab_2/main.py
@@ -1,14 +1,17 @@
 import argparse
 
 from cats import download_cats
+from csv_cats import create_csv
 
 def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('path_to_images', type=str, help='Path to save images')
+    parser.add_argument('path_to_csv', type=str, help='Path to csv')
     args = parser.parse_args()
 
     download_cats("cats", 10, args.path_to_images)
+    create_csv(args.path_to_images, args.path_to_csv)
 
 if __name__ == "__main__":
     main()
