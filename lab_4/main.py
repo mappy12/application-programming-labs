8b76f9648d7f9d45036a945d8f256f0b22bd0410
mappy-mac <mappydesuu@gmail.com>
2024-10-25 21:52:06 +0400

    added height, width, channels columns

--- lab_4/main.py
+++ lab_4/main.py
@@ -1,8 +1,34 @@
+import cv2
+import numpy as np
 import pandas as pd
+
+def get_dementions(path_to_image):
+
+    img = cv2.imread(path_to_image)
+
+    height, width, channels = img.shape
+    return height, width, channels
 
 def main():
     df = pd.read_csv("catsCsv")
-    print(df.head())
+
+    df.columns = ['Absolute Path', 'Relative Path']
+
+    heights = []
+    widths = []
+    channels_list = []
+
+    for index, row in df.iterrows():
+        height, width, channels =  get_dementions(row['Absolute Path'])
+
+        heights.append(height)
+        widths.append(width)
+        channels_list.append(channels)
+
+    df['Height'] = heights
+    df['Width'] = widths
+    df['Channels'] = channels_list
+
 
 if __name__ == "__main__":
     main()
