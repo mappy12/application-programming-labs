7e6f175690340977f3b300fafd518de0167e39cc
mappy-mac <mappydesuu@gmail.com>
2024-11-21 21:34:50 +0400

    added 'open-csv' button and file dialog

--- lab_5/main_window.py
+++ lab_5/main_window.py
@@ -1,19 +1,33 @@
 import sys
 
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import QSize
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog
+
 
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("Image Viewer")
+        self.setFixedSize(QSize(1280,700))
+        self.move(100,100)
 
+        self.setup_main_window()
 
-    def add_label(self):
-        self.new_text.setText("Привет!")
-        self.new_text.move(100, 50)
-        self.new_text.adjustSize()
+        self.csv_path = None
+
+    def setup_main_window(self):
+        opencsv_button = QPushButton("Open csv-file")
+        opencsv_button.clicked.connect(self.setup_file_dialog)
+
+        self.setCentralWidget(opencsv_button)
+        opencsv_button.setFixedSize(200,50)
+
+    def setup_file_dialog(self):
+        csv_path, _ = QFileDialog.getOpenFileName(self, "Select CSV-file", "", "CSV file (*.csv)")
+        if csv_path:
+            self.csv_path = csv_path
+
 
 def application():
     app = QApplication(sys.argv)
