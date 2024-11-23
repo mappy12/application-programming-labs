64824a935acd4a0ccbea159ddddd871b772bfc12
mappy-mac <mappydesuu@gmail.com>
2024-11-23 17:24:28 +0400

    final display images and iterate functionality

--- lab_5/iterator.py
+++ lab_5/iterator.py
@@ -9,11 +9,14 @@ class CatsIterator:
 
     def __init__(self, path_to_csv: str):
         self.path_to_csv = path_to_csv
+        self.image_paths = None
+        self.csvreader = None
 
     def __iter__(self):
 
         self.image_paths = open(self.path_to_csv)
         self.csvreader = csv.reader(self.image_paths)
+        next(self.csvreader)
 
         return self
 
--- lab_5/main_window.py
+++ lab_5/main_window.py
@@ -1,7 +1,7 @@
 import sys
 
 from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QLabel, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QLabel, QVBoxLayout, QWidget, QSizePolicy
 from PyQt5.QtGui import QPixmap
 
 from iterator import CatsIterator
@@ -23,41 +23,55 @@ class Window(QMainWindow):
 
 
     def setup_main_window(self):
+        central_widget = QWidget(self)
+        self.setCentralWidget(central_widget)
+
+        layout = QVBoxLayout()
+        central_widget.setLayout(layout)
+
         self.label = QLabel(self)
         self.label.setAlignment(Qt.AlignCenter)
-        self.label.setFixedSize(1280, 600)
+        self.label.setSizePolicy(
+            QSizePolicy.Expanding, QSizePolicy.Expanding
+        )
+        self.label.setStyleSheet("background-color: #424242; border: 2px solid #adaaaa; border-radius: 5px")
 
-        opencsv_button = QPushButton("Open csv-file")
-        opencsv_button.clicked.connect(self.setup_file_dialog)
+        self.opencsv_button = QPushButton("Open csv-file")
+        self.opencsv_button.clicked.connect(self.setup_file_dialog)
+        self.opencsv_button.setFixedSize(200,50)
 
-        self.setCentralWidget(opencsv_button)
-        opencsv_button.setFixedSize(200,50)
+        self.next_button = QPushButton("Next image")
+        self.next_button.clicked.connect(self.show_next_image)
+        self.next_button.setEnabled(False)
+        self.next_button.setFixedSize(200,50)
 
-        next_button = QPushButton("Next image")
-        next_button.clicked.connect(self.show_next_image)
+
+        layout.addWidget(self.label)
+        layout.addWidget(self.opencsv_button, alignment=Qt.AlignCenter)
+        layout.addWidget(self.next_button, alignment=Qt.AlignCenter)
 
     def setup_file_dialog(self):
         csv_path, _ = QFileDialog.getOpenFileName(self, "Select CSV-file", "", "CSV file (*.csv)")
         if csv_path:
             self.csv_path = csv_path
-            self.image_iterator = CatsIterator(self.csv_path)
+            self.image_iterator = iter(CatsIterator(self.csv_path))
             self.show_next_image()
+
+            self.next_button.setEnabled(True)
+            self.opencsv_button.setEnabled(False)
 
 
     def show_next_image(self):
         try:
            image = next(self.image_iterator)
-           current_image = image[0]
+           self.current_image = image[0]
            self.display_image(self.current_image)
         except StopIteration:
             self.label.setText("No more images")
 
     def display_image(self, image):
         pixmap = QPixmap(image)
-        self.label.setPixmap(pixmap.scaled(self.label.size(), aspectRatioMode=1))
-
-
-
+        self.label.setPixmap(pixmap.scaled(self.label.size(), Qt.KeepAspectRatio))
 
 
 def application():
